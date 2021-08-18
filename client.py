265f16a22df28cd6b7fe8bdb0462cef4a1edd3cc
BrOlliver <bruno_olliver14@hotmail.com>
2021-08-17 22:34:13 -0300

    initial commit

---
+++ client.py
@@ -0,0 +1,17 @@
+#Client.py subscribe -> portalClient.py
+
+import socket                          # Import socket module
+
+s = socket.socket()                    # Create a socket object
+host = socket.gethostname()            # Get local machine name
+port = 12345                           # Reserve a port for your service.
+
+s.connect((host, port))
+
+print("1")
+data = s.recv(1024)
+print(data.decode())
+print("2")
+data = s.recv(1024)
+print(data.decode())
+s.close()                              # Close the socket when done
