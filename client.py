7440ccd0ba12c9633b33fb7a835f49cc44b0db79
BrOlliver <bruno_olliver14@hotmail.com>
2021-08-18 22:24:34 -0300

    initial commit

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 #Client.py subscribe -> mqtt -> portalClient.py
-
+import base64
+#import codecs
+#import pickle
 import socket                          # Import socket module
 
 s = socket.socket()                    # Create a socket object
@@ -8,10 +10,80 @@ port = 12345                           # Reserve a port for your service.
 
 s.connect((host, port))
 
-print("1")
-data = s.recv(1024)
-print(data.decode())
-print("2")
-data = s.recv(1024)
-print(data.decode())
-s.close()                              # Close the socket when done
+def menu():
+    print('========MENU DO CLIENTE======\n')
+    print('1. Nova Tarefa\n')
+    print('2. Alterar tarefa existente\n')
+    print('3. Listar todas as tarefas\n')
+    print('4. Excluir tarefa existente\n')
+    print('5. Excluir todas as tarefas\n')
+    print('6. Sair\n')
+    opcao = input('\nDigite uma das opções acima: ')
+    return opcao
+
+
+def validaID():
+    while True:
+        usrid = input('Digite seu ID: ')
+        if not usrid.isdigit():
+            print('ID precisa ser numerico')
+        else:
+            break
+    return usrid
+
+
+def run():
+
+    while True:
+        opcao = menu()
+
+        #s.send(opcao.encode())
+
+        if opcao == '1':
+            usrid = validaID()
+            titulo = input('Titulo da Tarefa: ')
+            tarefa = input('Descrição da Tarefa: ')
+
+            dataTask = {'ID': usrid,'TITULO': titulo,'TAREFA': tarefa}
+
+            s.send(str(dataTask).encode('utf-8'))
+
+            #DECODIFICANDO PARA VER SE CODIFICOU
+            #base64_dict = base64.b64encode(encoded_dict)
+            #print(encoded_dict)
+
+#my_dict = {'name': 'Rajiv Sharma', 'designation': "Technology Supervisor"}
+#encoded_dict = str(my_dict).encode('utf-8')
+#base64_dict = base64.b64encode(encoded_dict)
+#print(base64_dict)
+
+#my_dict_again = eval(base64.b64decode(base64_dict))
+#print(my_dict_again)
+
+        else:
+            break
+
+        data = s.recv(1024)
+        print("\nMensagem recebida: ",data.decode())
+
+
+    print('\nFechando conexão do Cliente')
+    s.close()                              # Close the socket when done
+
+run()
+
+
+#client.py
+
+#!/usr/bin/python                      # This is client.py file
+
+#import socket                          # Import socket module
+
+#s = socket.socket()                    # Create a socket object
+#host = socket.gethostname()            # Get local machine name
+#port = 12345                           # Reserve a port for your service.
+
+#s.connect((host, port))
+#data = s.recv(1024)
+#print(data.decode())
+#s.close()
--- portalClient.py
+++ portalClient.py
@@ -1,19 +1,60 @@
 #portalClient.py publish -> mqtt -> client.py
 
 #Socket
-
+import base64
+#import pickle
 import socket                               # Import socket module
+import paho.mqtt as mqtt
+import time
 
 s = socket.socket()                         # Create a socket object
 host = socket.gethostname()                 # Get local machine name
 port = 12345                                # Reserve a port for your service.
 s.bind((host, port))                        # Bind to the port
 
+banco = dict()
+
+
 s.listen(5)                                 # Now wait for client connections.
 while True:
-   c, addr = s.accept()                     # Establish connection with client.
+   c, addr = s.accept()
    print('Got connection from', addr)
-   c.send('Thank you for connecting 1'.encode())
-   c.send('Thank you for connecting 2\n'.encode())
-   c.send('Come back often'.encode())
+                        # Establish connection with client.
+   base64_dict = c.recv(1024)
+   #msgCliente = data.decode()
+
+   my_dict_again = eval(base64.b64decode(base64_dict))
+   print(my_dict_again)
+   #print(base64_dict)
+   my_dict_again = eval(base64.b64decode(base64_dict))
+   print(my_dict_again)
+
+   # EXEMPLO
+   #my_dict = {'name': 'Rajiv Sharma', 'designation': "Technology Supervisor"}
+   #encoded_dict = str(my_dict).encode('utf-8')
+   #base64_dict = base64.b64encode(encoded_dict)
+   #print(base64_dict)
+
+   #my_dict_again = eval(base64.b64decode(base64_dict))
+   #print(my_dict_again)
+
+
    c.close()
+
+
+#server.py
+#!/usr/bin/python                           # This is server.py file
+
+#import socket                               # Import socket module
+
+#s = socket.socket()                         # Create a socket object
+#host = socket.gethostname()                 # Get local machine name
+#port = 12345                                # Reserve a port for your service.
+#s.bind((host, port))                        # Bind to the port
+
+#s.listen(5)                                 # Now wait for client connections.
+#while True:
+#   c, addr = s.accept()                     # Establish connection with client.
+#   print('Got connection from', addr)
+#   c.send('Thank you for connecting'.encode())
+#   c.close()                                # Close the connection
